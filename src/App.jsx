fc883705b27645e97c96828c2ac005fc8ded91fb
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-06-06 16:38:33 +0200

    Feat: Milestone 1 completato: lista musicisti creata

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 // IMPORT //
+import jazzMusicians from "./data/jazzMusicians";
 
 function App() {
-
   return (
     <>
+      <div>
+        <h1>Musicisti Jazz</h1>
+        <ul>
+          {jazzMusicians.map((musicista) => (
+            <li key={musicista.id}>
+              {musicista.musicista}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ src/data/jazzMusicians.js
@@ -0,0 +1,34 @@
+const jazzMusicians = [
+  {
+    id: 1,
+    musicista: "Miles Davis",
+    strumenti: ["Tromba", "Flicorno"],
+    canzoni: ["Kind of Blue", "So What", "Blue in Green", "All Blues", "Freddie Freeloader"]
+  },
+  {
+    id: 2,
+    musicista: "John Coltrane",
+    strumenti: ["Sassofono tenore", "Sassofono soprano"],
+    canzoni: ["Giant Steps", "A Love Supreme", "My Favorite Things", "Blue Train", "Naima"]
+  },
+  {
+    id: 3,
+    musicista: "Bill Evans",
+    strumenti: ["Pianoforte"],
+    canzoni: ["Waltz for Debby", "Autumn Leaves", "Some Other Time", "Peace Piece", "Blue in Green"]
+  },
+  {
+    id: 4,
+    musicista: "Charlie Parker",
+    strumenti: ["Sassofono alto"],
+    canzoni: ["Ornithology", "Ko Ko", "Now's the Time", "Yardbird Suite", "Confirmation"]
+  },
+  {
+    id: 5,
+    musicista: "Ella Fitzgerald",
+    strumenti: ["Voce"],
+    canzoni: ["Summertime", "Dream a Little Dream of Me", "Mack the Knife", "A-Tisket, A-Tasket", "Cheek to Cheek"]
+  }
+];
+
+export default jazzMusicians;
